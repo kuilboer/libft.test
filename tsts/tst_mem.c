1e0ecb384d0e97b95df8f65956c14e9e9433e8c1
Olaf <okuilboe@student.codam.nl>
2025-05-04 21:21:09 +0200

    Working on memcpy() tests. switching to mobile workstation.

--- tsts/tst_mem.c
+++ tsts/tst_mem.c
@@ -6,11 +6,39 @@
 /*   By: okuilboe <okuilboe@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/28 15:35:57 by okuilboe      #+#    #+#                 */
-/*   Updated: 2025/05/04 19:39:40 by okuilboe      ########   odam.nl         */
+/*   Updated: 2025/05/04 21:18:02 by okuilboe      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tsts.h"
+
+static int run_memcpy_test(const void *src_data, size_t len, size_t n)
+{
+	/* create sys_d and ft_d buffers for comparison with a length of (size_t)len */
+	
+	unsigned char	*src = (unsigned char *)src_data;
+	unsigned char	*dst_sys = calloc(1, len);
+	unsigned char	*dst_ft = calloc(1, len);
+	if (!sys_d || !ft_d)
+		return (-1);
+
+	void	*ret_sys = memcpy(dst_sys, src, n);
+	void	*ret_ft = memcpy(dst_ft, src, n);
+
+	if (memcmp(ret_ft, ret_sys, n) != 0 || ret_ft != dst_ft)
+	{
+		for (size_t i = 0; i < len; i++)
+		{
+			printf("Byte %02zu: std = %02X, ft = %02X\n", i, dst_sys[i], dst_ft[i]);
+		}
+		free(dst_sys);
+		free(dst_ft);
+		return (1); //FAIL
+	}
+	free(dst_sys);
+	free(dst_ft);
+	return (0); //PASS
+}
 
 static int run_memset_test(void *sys_s, void *ft_s, int c, size_t n)
 {
@@ -32,7 +60,62 @@ static int run_memset_test(void *sys_s, void *ft_s, int c, size_t n)
 	return (0); //PASS
 }
 
+int test_ft_memcpy(void)
+{
+	// int		i = 0;
+	// unsigned char	src[] = {0XAA, 0XFF, 0X00, 0xDE, 0xAD, 0xBE, 0xEF, '\0'};
+	// size_t			n[] = {0, 1, 10, 60};
+	// unsigned char	sys_d[50];
+	// unsigned char	ft_d[50];
 
+	// memset(sys_s, 0X00, 50);
+	// memset(ft_s, 0X00, 50);
+	// i = 0;
+    // while (c[i] != '\0')
+    // {
+	// 	memset(sys_s, 0X00, 50);
+	// 	memset(ft_s, 0X00, 50);
+	// 	int ni = (sizeof(n)/sizeof(n[0]))-1;
+	// 	while (ni > 0)
+	// 	{
+	// 		if (run_memset_test(sys_s, ft_s, c[i], n[ni]))
+	// 			return (1); // fail
+	// 		ni--;
+	// 	}
+	// 	i++;
+
+	unsigned char src_data[32];
+	for (size_t i = 0; i < sizeof(src_data); i++)
+		src_data[i] = (unsigned char)(i + 1);  // Fill with 0x01, 0x02, ..., 0x20
+
+	int fails = 0;
+
+	// Test various lengths
+	size_t test_sizes[] = {0, 1, 5, 16, 31, 32};
+	size_t num_sizes = sizeof(test_sizes) / sizeof(test_sizes[0]);
+
+	i = 0 ;
+	while (condition)
+	{
+		/* code */
+	}
+	
+	for (size_t i = 0; i < num_sizes; i++)
+	{
+		if (test_ft_memcpy_case(src_data, sizeof(src_data), test_sizes[i]) != 0)
+			fails++;
+	}
+
+	if (fails == 0)
+		printf("✅ All memcpy tests passed!\n");
+	else
+		printf("❌ %d memcpy test(s) failed.\n", fails);
+
+	return fails;
+
+    }
+    return (0); // pass
+}
 
 int test_ft_memset(void)
 {
